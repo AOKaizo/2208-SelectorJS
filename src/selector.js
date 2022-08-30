7fbfcc12848dc1007a80b10cc54885741c791127
AOKaizoMac <kolbywolf@gmail.com>
2022-08-30 11:37:58 -0400

    Tag and Class Dual Selector Complete

--- src/selector.js
+++ src/selector.js
@@ -8,10 +8,13 @@ class $ {
     let storedClass = [...preppedClass];
     let preppedTag = document.getElementsByTagName(CSSId);
     let storedTag = [...preppedTag];
-    if (CSSId.includes('.')) {
-        let preppedTag = document.getElementsByTagName(CSSId.slice(0, CSSId.indexOf('.')));
-        storedTag = [...preppedTag].filter((element) => element.includes(CSSId.slice(CSSId.findIndex(element => element === '.')+1)))
-        console.log(storedTag)
+    if (CSSId.includes(".")) {
+      let preppedTag = document.getElementsByTagName(
+        CSSId.slice(0, CSSId.indexOf("."))
+      );
+      storedTag = [...preppedTag].filter(
+        (element) => element.className === CSSId.slice(CSSId.indexOf(".") + 1)
+      );
     }
     if (CSSId[0] === ".") {
       this.elements = storedClass;
@@ -85,27 +88,27 @@ class $ {
 //       this.elements.forEach((element) => (element.style.display = "none"));
 //       return this;
 //     };
-  
+
 //     this.show = () => {
 //       this.elements.forEach((element) => (element.style.display = "inherit"));
 //       return this;
 //     };
-  
+
 //     this.addClassName = (classToAdd) => {
 //       this.elements.forEach((element) => (element.classList = classToAdd));
 //       return this;
 //     };
-  
+
 //     this.removeClassName = (classToRemove) => {
 //       this.elements.forEach((element) => element.classList.remove(classToRemove));
 //       return this;
 //     };
-  
+
 //     this.text = (textToAdd) => {
 //       this.elements.forEach((element) => (element.innerText = textToAdd));
 //       return this;
 //     };
-  
+
 //     this.addChild = (nodeToAdd) => {
 //       this.elements.forEach((element) =>
 //         element.appendChild(document.createElement(nodeToAdd))
