3838520a883bdf23317117dc85de2dcede60dac5
AOKaizoMac <kolbywolf@gmail.com>
2022-08-29 23:09:34 -0400

    Complete and Working!

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,50 @@
 // YOUR CODE HERE
+
+class $ {
+  constructor(CSSId) {
+    let preppedId = document.getElementById(CSSId.slice(1));
+    let storedId = [preppedId];
+    let preppedClass = document.getElementsByClassName(CSSId.slice(1));
+    let storedClass = [...preppedClass];
+    let preppedTag = document.getElementsByTagName(CSSId);
+    let storedTag = [...preppedTag];
+    if (CSSId[0] === ".") {
+      this.elements = storedClass;
+    } else if (CSSId[0] === "#") {
+      this.elements = storedId;
+    } else {
+      this.elements = storedTag;
+    }
+  }
+
+  hide() {
+    this.elements.forEach((element) => (element.style.display = "none"));
+    return this;
+  }
+
+  show() {
+    this.elements.forEach((element) => (element.style.display = "inherit"));
+    return this;
+  }
+  addClassName(classToAdd) {
+    this.elements.forEach((element) => (element.classList = classToAdd));
+    return this;
+  }
+
+  removeClassName(classToRemove) {
+    this.elements.forEach((element) => element.classList.remove(classToRemove));
+    return this;
+  }
+
+  text(textToAdd) {
+    this.elements.forEach((element) => (element.innerText = textToAdd));
+    return this;
+  }
+
+  addChild(nodeToAdd) {
+    this.elements.forEach((element) =>
+      element.appendChild(document.createElement(nodeToAdd))
+    );
+    return this;
+  }
+}
